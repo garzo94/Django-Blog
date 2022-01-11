cfecad8a3e67e4f2238ed9cf7f6b4489f83f02a2
garzo94 <alexgarzo25@gmail.com>
2022-01-10 18:35:48 -0600

    structure blog

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(admin.ModelAdmin):
  list_display = ('title', 'slug', 'author', 'publish', 'status')
  list_filter = ('status', 'created', 'publish', 'author')
  search_fields = ('title', 'body')
- prepopulated_fields = {'slug': ('title',)} #fill the slug field automatically when you write your tile down (When creating blog)
+ prepopulated_fields = {'slug': ('title',)} #fill the slug field automatically when you write your tile down (When creating blog (POST))
  raw_id_fields = ('author',) #display a lookup, very usefull when you a tons of user
  date_hierarchy = 'publish'  #Just below the search bar, there are navigation links to navigate through a date hierarchy; this has been defined by the date_hierarchy attribute
  ordering = ('status', 'publish')
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+#this for get an absolute url
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -11,7 +13,7 @@ class Post(models.Model):
         )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,  # This is a field intended to be used in URLs
-        unique_for_date='publish')
+        unique_for_date='publish') # This ensures that there will be only one post with a slug for agiven date, and thus, you can retrieve single posts using the date and slug
     author = models.ForeignKey(User,   #: This field defines a many-to-one relationship, user can write any number of posts
         on_delete=models.CASCADE, # Using CASCADE, you specify that when the referenced user is deleted, the database will also delete all related blogposts.
         related_name='blog_posts') # "related_name" will allow you to access related objects easily.
@@ -22,6 +24,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10, # You use a choices parameter,so the value of this field can only be set to one of the given choices.
         choices=STATUS_CHOICES,
         default='draft')
+
+    def get_absolute_url(self):  # to link to specific posts.
+        return reverse('blog:post_detail',
+        args=[self.publish.year,
+        self.publish.month,
+        self.publish.day, self.slug])
 
     # With Meta class  You tell Django to sort results by the publish field in descending order by default when you query the database.
     #You specify the descending order using the negative prefix. By doing this, posts
